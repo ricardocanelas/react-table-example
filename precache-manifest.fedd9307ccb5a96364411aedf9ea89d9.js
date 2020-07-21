37d44332b7f232e0312385b36cecc32e6b012e9d
Ricardo Canelas <ricardocanelas@gmail.com>
2020-07-21 13:56:05 -0300

    Updates

---
+++ precache-manifest.fedd9307ccb5a96364411aedf9ea89d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022c02d167695498f0836aa9a0e68763",
+    "url": "/react-table-example/index.html"
+  },
+  {
+    "revision": "e2646659039f40d2fafb",
+    "url": "/react-table-example/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "7537a34b60f6aedfb6b8",
+    "url": "/react-table-example/static/css/main.ab9c3133.chunk.css"
+  },
+  {
+    "revision": "e2646659039f40d2fafb",
+    "url": "/react-table-example/static/js/2.eb8c8b95.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/react-table-example/static/js/2.eb8c8b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7537a34b60f6aedfb6b8",
+    "url": "/react-table-example/static/js/main.8c88dc9e.chunk.js"
+  },
+  {
+    "revision": "33e1d9bc96adce21ff46",
+    "url": "/react-table-example/static/js/runtime-main.0d716298.js"
+  }
+]);
